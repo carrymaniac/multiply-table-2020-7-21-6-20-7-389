5fe1257f63796f7d863e12acb5228baf758476ed
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-21 16:17:33 +0800

    “finish the createMultiplyTable function”

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 function createMultiplyTable(start, end) {
-  return null;
+  return isVail?generateMultiplyAllLine(start, end) : "null";
+}
+
+function isVail(start, end){
+    return (start<end)&&(start>0&&start<=1000)&&(end>0&&end<=100);
+}
+
+function generateMultiplyAllLine(start, end){
+   const result = new Array();
+   for(let i = start; i<=end; i++){
+      result.push(generateMultiplyOneLine(start,i));
+   }
+   return connectMultiplyOneLine(result);
+}
+
+function generateMultiplyOneLine(lineStart,lineEnd){
+    let str = "";
+    for(let i = lineStart; i<=lineEnd; i++){
+        str += ""+i+"*"+lineEnd+"="+(i*lineEnd);
+        str += "\t"
+    }
+    str = str.substring(0,str.length-1);
+    return str;
+}
+
+function connectMultiplyOneLine(resultArr){
+    return resultArr.join("\n");
 }
 
 module.exports = {
